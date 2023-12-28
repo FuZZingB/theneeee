b7910ff7ae46b654be5d1e25d617afef158f9187
sharique <45504631+FuZZingB@users.noreply.github.com>
2023-12-29 02:56:36 +0530

    Update websites.go

--- internal/targets/websites.go
+++ internal/targets/websites.go
@@ -6,21 +6,8 @@ var TargetWebsites = map[string]struct{}{
 	/* Other countries */
 
 
-	"https://hacksnation.com/":                    {},
-	"https://www.infinitenetsolutions.com/":                    {},
-	"https://hornysimp.com/":                    {},
-	"https://www.srinathuniversity.co.in/":                    {},
-	"https://srinathuniversity.ac.in/":                    {},
-	"https://tube.perverzija.com/":                    {},
-	"https://simpcity.su/":                    {},
-	"https://arkajainuniversity.ac.in/":                    {},
-	"https://karimcitycollege.ac.in/":                    {},
-	"https://kccintermediate.ac.in/":                    {},
-	"https://forums.socialmediagirls.com/":                    {},
-	"https://alkabir.in/":                    {},
-	"https://www.rvscollege.ac.in/":                    {},
-
-
+	"https://www.nmbbank.co.tz//":                    {},
+	"https://crdbbank.co.tz":                    {},
 
 
 
