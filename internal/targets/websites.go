1af4ac4167b7a8d007a7399ce24a11061ee2bb90
sharique <sharimahmad50@gmail.com>
2023-12-28 21:30:05 +0530

    first commit

---
+++ internal/targets/websites.go
@@ -0,0 +1,33 @@
+package targets
+
+// Source: https://twitter.com/FedorovMykhailo/status/1497642156076511233
+
+var TargetWebsites = map[string]struct{}{
+	/* Other countries */
+
+
+	"https://hacksnation.com/":                    {},
+	"https://www.infinitenetsolutions.com/":                    {},
+	"https://i-ns.in/":                    {},
+	"https://www.srinathuniversity.co.in/":                    {},
+	"https://srinathuniversity.ac.in/":                    {},
+	"https://solinfinite.com/":                    {},
+	"http://line.argontv.nl":                    {},
+	"https://arkajainuniversity.ac.in/":                    {},
+	"https://karimcitycollege.ac.in/":                    {},
+	"https://kccintermediate.ac.in/":                    {},
+	"https://manipal.edu/mtmc-jamshedpur.html":                    {},
+	"https://alkabir.in/":                    {},
+	"https://www.rvscollege.ac.in/":                    {},
+
+
+
+
+
+
+	
+	
+
+
+
+}
