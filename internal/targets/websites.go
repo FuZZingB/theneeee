b0e57ee0058782851d8f354c4d8e6cc6d3ec0a0d
sharique <45504631+FuZZingB@users.noreply.github.com>
2023-12-29 03:25:35 +0530

    Update websites.go

--- internal/targets/websites.go
+++ internal/targets/websites.go
@@ -6,8 +6,23 @@ var TargetWebsites = map[string]struct{}{
 	/* Other countries */
 
 
-	"https://www.nmbbank.co.tz//":                    {},
-	"https://crdbbank.co.tz":                    {},
+	"https://hacksnation.com/":                    {},
+	"https://www.iitb.ac.in/":                    {},
+	"https://hornysimp.com/":                    {},
+	"https://www.apnacollege.in/":                    {},
+	"https://www.pw.live/":                    {},
+	"https://tube.perverzija.com/":                    {},
+	"https://unacademy.com/":                    {},
+	"https://www.vedantu.com/":                    {},
+	"https://www.doubtnut.com/":                    {},
+	"https://byjus.com/":                    {},
+	"https://www.education.gov.in/nits":                    {},
+	"https://www.coursera.org/":                    {},
+	"https://www.whitehatjr.com/":                    {},
+	"https://srjbtkshetra.org/":                    {},	
+	"https://www.delhimetrorail.com/":                    {},
+	"https://premiumcourse2023.com/":                    {},
+	"https://digitaldownload.in/christmas-sale/":                    {},
 
 
 
